e6e65a3ca89b4eb8baae2a4aecfdbd11acb022e6
Anatoly Koshlyak <air721@yandex.ru>
2023-10-18 19:05:13 +0600

    Изменит ф-ю checkStringIsPalindrom на стрелочную

--- js/functions.js
+++ js/functions.js
@@ -17,8 +17,11 @@ checkLengthString('Строка для проверки', 19);
 
 checkLengthString('Строка для проверки', 10);
 
-
-const checkStringIsPalindrom = function (str) {
+/* Функция проверки является ли слово или фраза палиндромом(читается одинаково как с начала так и с конца).
+Если слово или фраза, которую передали в функцию (str) является палиндромом,
+то функция вернёт true. В противном случае функция вернёт false.
+*/
+const checkStringIsPalindrom = (str) => {
   const string = str.replaceAll(' ', '').toLowerCase();
   let newString = '';
   for (let i = string.length - 1; i >= 0; i --) {
