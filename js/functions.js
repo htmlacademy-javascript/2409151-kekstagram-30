ef37801f33cc851a05edf7814cfb17fde27afd19
Anatoly Koshlyak <air721@yandex.ru>
2023-10-18 15:28:06 +0600

    Добавит строку checkStringIsPalindrom

--- js/functions.js
+++ js/functions.js
@@ -9,9 +9,28 @@ const checkLengthString = function (str, maxLength) {
   return false;
 };
 
-// eslint-disable-next-line
-console.log(checkLengthString('Строка для проверки', 20));
-// eslint-disable-next-line
-console.log(checkLengthString('Строка для проверки', 19));
-// eslint-disable-next-line
-console.log(checkLengthString('Строка для проверки', 10));
+//const checkLengthString = (str, maxLength) => str.length <= maxLength;
+
+checkLengthString('Строка для проверки', 20);
+
+checkLengthString('Строка для проверки', 19);
+
+checkLengthString('Строка для проверки', 10);
+
+
+const checkStringIsPalindrom = function (str) {
+  const string = str.replaceAll(' ', '').toLowerCase();
+  let newString = '';
+  for (let i = string.length - 1; i >= 0; i --) {
+    newString += string[i];
+  }
+  return newString === string;
+};
+
+checkStringIsPalindrom('Т о п о p');
+checkStringIsPalindrom('Т о п о т');
+checkStringIsPalindrom('Лёша на полке клопа нашёл');
+checkStringIsPalindrom('Кучук');
+checkStringIsPalindrom('ДОВОД');
+checkStringIsPalindrom('Повод');
+
