335d6f03c9f83d1d31ebd63b6d23c079ddc477e8
Anatoly Koshlyak <air721@yandex.ru>
2023-11-12 12:17:31 +0600

    Перенёс часть функций из utils.js

--- js/data.js
+++ js/data.js
@@ -15,6 +15,26 @@ const NAMES = ['Кирилл', 'Александр', 'Ольга', 'Олег', '
   'Владимир', 'Сергей', 'Марина', 'Константин', 'Дмитрий', 'Иван', 'Максим', 'Руслан', 'Андрей', 'Евгений', 'Виктор',
   'Снежана', 'Мария', 'Валентина', 'Светлана'];
 
+const POST_ID_COUNTER = 25;
+const COMMENT_COUNTER = 30;
+const COMMENT_ID_COUNTER = 3333;
+
+/*Создаём массив целых последовательных чисел для генерации id поста. Далее, функцией getRandomInteger(), извлекаем
+  из него случайным образом числа.
+*/
+const postIds = [];
+for (let i = 1; i <= POST_ID_COUNTER; i++) {
+  postIds.push(i);
+}
+
+/*Создаём массив целых последовательных чисел для генерации id комментария. Далее, функцией getRandomInteger(), извлекаем
+  из него случайным образом числа.
+*/
+const commentIds = [];
+for (let i = 1; i <= COMMENT_ID_COUNTER; i++) {
+  commentIds.push(i);
+}
+
 const getDescriptionPhotos = () => (
   DESCRIPTION_PHOTOS[getRandomInteger(0, DESCRIPTION_PHOTOS.length - 1)]
 );
@@ -27,4 +47,34 @@ const getName = () => (
   NAMES[getRandomInteger(0, NAMES.length - 1)]
 );
 
-export {getDescriptionPhotos, getMessage, getName};
+//функция создаёт один объект с комментариями
+const getComment = () => {
+  const index = getRandomInteger(0, commentIds.length - 1);
+  const commentId = commentIds[index];
+  commentIds.splice(index, 1);
+  return {
+    id: commentId,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getMessage(),
+    name: getName()
+  };
+};
+
+//Функция createTemporaryData создаёт один объект
+const createTemporaryData = () => {
+  const index = getRandomInteger(0, postIds.length - 1);
+  const id = postIds[index];
+  const url = postIds[index];
+  postIds.splice(index, 1);
+  return {
+    id: id,
+    url: `photos/'${url}.jpg`,
+    description: getDescriptionPhotos(),
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({length: getRandomInteger(0, COMMENT_COUNTER)}, getComment)
+  };
+};
+
+const createArrayOfPosts = () => Array.from({length: POST_ID_COUNTER}, createTemporaryData);
+
+export {createArrayOfPosts};
